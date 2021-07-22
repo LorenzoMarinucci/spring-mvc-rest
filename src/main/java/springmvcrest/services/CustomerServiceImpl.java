477094db7eafe293aa43f6c78de40570b69d807e
LorenzoMarinucci <lorenmarinucci@gmail.com>
2021-07-22 11:15:02 -0300

    Patch operation

--- src/main/java/springmvcrest/services/CustomerServiceImpl.java
+++ src/main/java/springmvcrest/services/CustomerServiceImpl.java
@@ -73,7 +73,10 @@ public class CustomerServiceImpl implements CustomerService {
                 customer.setLastName(customerDTO.getLastName());
             }
 
-            return customerMapper.customerToCustomerDTO(customerRepository.save(customer));
+            CustomerDTO returnDTO = customerMapper.customerToCustomerDTO(customerRepository.save(customer));
+            returnDTO.setCustomerUrl(URI + id);
+
+            return returnDTO;
         }).orElseThrow(RuntimeException::new);
     }
 
